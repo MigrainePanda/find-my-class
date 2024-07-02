fbe19daa719b3484abf36952b6f4c65f00bd4119
MigrainPanda <nicktanaka36@gmail.com>
2024-07-01 22:05:35 -0700

    fetch courses done

---
+++ backend/courses.js
@@ -0,0 +1,77 @@
+import { db, makeRequest } from './setup.js'
+
+
+
+const COURSES_URL = "https://classes.oregonstate.edu/api/?page=fose&route=search"
+
+
+
+async function fetchCoursesByKeyword(keyword) {
+  const requestBody = {
+    other: {
+      srcdb: "999999"
+    },
+    criteria: [
+      {
+        field: "alias",
+        value: keyword
+      }
+    ]
+  };
+
+  const courses = await fetchCourseHelper(requestBody);
+  return courses;
+}
+
+async function fetchCourseByCRN(crn) {
+  const requestBody = {
+    other: {
+      srcdb: "999999"
+    },
+    criteria: [
+      {
+        field: "crn",
+        value: crn
+      }
+    ]
+  };
+  const course = await fetchCourseHelper(requestBody);
+  return course;
+}
+
+async function fetchCourseByTerm(term) {
+  const requestBody = {
+    other: {
+      srcdb: "999999"
+    },
+    criteria: [
+      {
+        field: "srcdb",
+        value: term
+      },
+      {
+        field: "stat",
+        value: "A",
+      }
+    ]
+  };
+  const course = await fetchCourseHelper(requestBody);
+  return course;
+}
+
+async function fetchCourseHelper(requestBody) {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const payload = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(requestBody)
+  }
+
+  const response = await makeRequest(COURSES_URL, payload);
+  return response;
+}
+
+
+export { fetchCoursesByKeyword, fetchCourseByCRN, fetchCourseByTerm };
