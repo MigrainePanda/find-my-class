6dd06be76205ce9726c690ac93cee36bd18b6d70
MigrainPanda <nicktanaka36@gmail.com>
2024-07-04 16:43:05 -0700

    added footer and home page description

---
+++ react-find-my-class/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Nicholas Tanaka. All rights reserved.</p>
+        </footer>
+    );
+  }
+  
+  export default Footer;
